a813dcfba93b7846a413c8c581b15039e2a14e92
Full-Stack Developer <88977217+ryck-developer@users.noreply.github.com>
2023-10-27 05:26:43 -0300

    Add files via upload

---
+++ js/links.js
@@ -0,0 +1,14 @@
+export const links = [
+  {
+    name: 'Whatsapp',
+    link: 'https://api.whatsapp.com/send?phone=5586994059642',
+  },
+  {
+    name: 'Cursos Plano Mensal',
+    link: 'https://www.cod3r.com.br/bundles/plano-de-assinatura?ref=5eebf4',
+  },
+  {
+    name: 'Curso de Sites',
+    link: 'https://www.cod3r.com.br/courses/nextjs?ref=5eebf4',
+  },
+];
